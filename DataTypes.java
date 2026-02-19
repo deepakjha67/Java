7d90cbdab4f5297b7f25ad0e95027746f3f2bfec
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-02-19 23:11:30 +0530

    Add  DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String args[]) {
+      
+        int age=22;
+        System.out.println(age);
+
+        float pi = 3.14f;
+        System.out.println(pi);
+
+        long num = 2147483649L;
+        System.out.println(num);
+
+        char c = 'c';
+        System.out.println(c);
+
+        boolean isSubscribed = true;
+        System.out.println(isSubscribed);
+      
+    }
+}
