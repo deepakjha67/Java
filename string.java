c61724c06177033cb935756eb8ae9d582d3752fb
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-02-20 18:36:36 +0530

    Add example of String manipulation in Java
    Demonstrate usage of String methods in Java.

---
+++ string.java
@@ -0,0 +1,17 @@
+
+// Non Primitive Data Types
+
+public class string {
+    public static void main(String args[]) {
+        String name = "Be consistent";
+        String other = " Work Daily";
+
+        System.out.println(name + other.toUpperCase());
+        System.out.println(other.toUpperCase());
+        System.out.println(name.charAt(2));
+    }
+}
+
+//Output: 
+// Be consistent WORK DAILY
+// WORK DAILY
