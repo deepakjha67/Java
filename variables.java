ede916c3ee82b6faaec3d9f941f77ec9a0816237
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-02-21 23:20:17 +0530

    Create variables.java to demonstrate variable handling
    Added a Java class to demonstrate variable usage and scope.

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main (String args[]) {
+        int age = 22;
+        System.out.println(age);
+
+        int x = 56;
+        System.out.println(x);
+
+        age = 45;
+        System.out.println(age); //system will print the new value of age which is 45
+        
+        int Age = 22; // age , AGE and Age are different variables
+        System.out.println(Age);
+
+    }
+}
+// Output
+// 22
+// 56
+// 45
+// 22
