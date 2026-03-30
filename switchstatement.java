31baeea4082892dcd4d61c48ea34b76bd65f0ae7
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-03-30 23:01:14 +0530

    Update switchstatement.java

--- switchstatement.java
+++ switchstatement.java
@@ -1,45 +1,48 @@
-// Use of switch and Break statements in java
-
 public class switchstatement {
-    public static void main(String[] args) {
-        int opt = 1;
-        
-        switch (opt)  {
-        //     case 1:
-        //         System.out.println("That you selected the english language");
-            
 
-        //     case 2:
-        //         System.out.println("You selescted Hindi language");
-
-        //     case 3:
-        //         System.out.println("You selected French Language");    
-     
-
-        // Output :
-        // You selescted Hindi language
-        // You selected French Language 
-            
-
-        
-            case 1:
-                System.out.println("That you selected the english language");
-                break;
-
-            case 2:
-                System.out.println("You selescted Hindi language");
-                break;
-            case 3:
-                System.out.println("You selected French Language"); 
-                break;
-            default:
-                System.out.println("Invalid Option");   // When no case is selected    
-     
-             // Output :
-             // You selescted Hindi language
-             // You selected French Language 
-             
-        }
-
-    }
-} 
+	public static void main(String[] args) {
+//		int n=1;
+//		if(n==1)
+//			System.out.println("Monday");
+//		else if(n==2)
+//			System.out.println("Tuesday");
+//		else if(n==3)
+//		System.out.println("Wednesday");
+//		else if(n==4)
+//		System.out.println("Thursday");
+//		else if(n==5)
+//		System.out.println("Friday");
+//		else if(n==6)
+//		System.out.println("Saturday");
+//		else
+//		System.out.println("Sunday");
+		
+		int n=8;
+		switch(n) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+		System.out.println("Wednesday");
+			break;
+		case 4:
+		System.out.println("Thursday");
+			break;
+		case 5:
+		System.out.println("Friday");
+			break;
+		case 6:
+		System.out.println("Saturday");
+			break;
+		case 7:
+		System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Enter a valid number");			
+		}
+		
+	}
+}
