f603298741778bfe6c3c496df71eff5c2f3559c5
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-03-31 22:16:23 +0530

    Create literals.java

---
+++ literals.java
@@ -0,0 +1,25 @@
+public class literals {
+    public void main (String args[]) {
+        
+        // literals
+
+        int num1 = 0b101;
+        System.out.println(num1);     // Output : 5
+
+        int num2 = 10_00_00_00_00;
+        System.out.println(num2);     // Output: 1000000000
+
+        double num3 = 12e10;
+        System.out.println(num3);
+
+        char c = 'a';
+        System.out.println(c);       // Output: a
+
+
+        c++;
+        System.out.println(c);       // Output: b
+
+   
+
+    }   
+}
