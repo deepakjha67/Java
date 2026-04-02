fa589f53326af107426ca7692fc26daea4f91b5b
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-02 18:32:01 +0530

    Create RelationalOperators.java

---
+++ RelationalOperators.java
@@ -0,0 +1,13 @@
+public class RelationalOperators {
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 5;
+		
+		int x = 9;
+		int y = 8;
+		
+		boolean result = x >= b ;
+		System.out.println(result);       // Output : True
+		// != , < , > , = 
+	}
+}
