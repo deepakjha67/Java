a4249656542c3ab169a11951bb934bde6079723d
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-02 18:36:01 +0530

    Create IfElseStatement.java

---
+++ IfElseStatement.java
@@ -0,0 +1,21 @@
+public class IfElseStatement {
+	public static void main(String args [] )
+	{
+		
+		int  a = 6;
+		int b = 8;
+		
+		int x = 9;
+		int y = 2;
+		
+		if (a<b && x>y)
+		System.out.println(" Hellow");
+		
+		else
+		System.out.println(" Bye");
+		
+		
+		
+		}
+	
+	}
