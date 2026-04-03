2ca891b6322be1c8e58ac4f9cc065b2eb8ac591e
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-03 10:46:06 +0530

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,16 @@
+public class TernaryOperator {
+	public static void main(String[] args) {
+		int n= 10;
+		int result = 0;
+		
+		result = n%2 == 0 ? 10 : 20;
+		
+		// if true " ? "
+		// if false " : "
+		
+		System.out.println(result);
+		 
+	}
+}
+
+// Output : 10
