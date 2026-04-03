c34abeaf1e490eed9188de968cec757b1cec2459
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-03 22:40:04 +0530

    Create DoWhileLoop.java
    Corrected the do-while loop structure and output comment.

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main (String args[])
+    {
+        int i = 5;
+        do         
+        {
+            System.out.println("Hellow " +i);
+            i++;
+        } while (i<=4); 
+    }
+}
+// Output : Hellow 5
