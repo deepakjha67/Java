2a92796073d4cbff6846dc12dae42838d2bb2a62
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-14 16:33:06 +0530

    Add SimpleInterest.java to calculate simple interest
    This program calculates simple interest based on user input for principal, rate, and time.

---
+++ Practice/SimpleInterest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class SimpleInterest {
+    public static void main (String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the principal, Rate, Time: ");
+
+        double principal = input.nextDouble();
+        double Rate = input.nextDouble();
+        double Time = input.nextDouble();
+
+        double si = (principal*Time*Rate)/100;
+
+        System.out.println("Simple interest: " + si);
+        input.close();
+
+    }
+    
+}
+/*
+Output :
+Enter the principal, Rate, Time: 
+2
+3
+4
+Simple interest: 0.24
+*/
