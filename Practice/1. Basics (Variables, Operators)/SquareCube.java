ba60e0bbed34fbad5c61517e9b2a4ff0122ae81a
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-17 17:46:29 +0530

    Create SquareCube.java

---
+++ Practice/1. Basics (Variables, Operators)/SquareCube.java
@@ -0,0 +1,13 @@
+// Problem: Find square and cube of a number
+import java.util.Scanner;
+
+public class SquareCube {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Square = " + (num * num));
+        System.out.println("Cube = " + (num * num * num));
+        sc.close();
+    }
+}
