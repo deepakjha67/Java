cf37c82c2e24e498c6ba31302cd67701a679553a
Deepak kumar jha <228211925+deepakjha67@users.noreply.github.com>
2026-04-22 10:58:43 +0530

    Create Numbers.java

---
+++ Practice/2. Control Flow (if-else, loops)/Numbers.java
@@ -0,0 +1,25 @@
+// Positive, negative or zero
+
+import java.util.Scanner;
+
+public class Numbers
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int a = sc.nextInt();
+
+        if (a>0) System.out.println("positive");
+
+        else if (a<0) System.out.println("Negative");
+        else System.out.println("Zero");
+        sc.close();
+    }
+}
+/*
+Output :
+Enter the number: 
+67 
+positive
+*/
